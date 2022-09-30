127f636d2695a179e9b3459616eba19fd5165c5b
santosh2311 <santoshkumarjhd11@gmail.com>
2022-09-30 14:42:16 +0530

    deploy

--- implementation/backend.tf
+++ implementation/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "santosh-test-tf"
-    key    = "new.tfstate"
-    region = "us-east-1"
-    dynamodb_table = "santosh-demo-b5"
+    # bucket = "santosh-test-tf"
+    # key    = "new.tfstate"
+    # region = "us-east-1"
+    # dynamodb_table = "santosh-demo-b5"
   }
 }
